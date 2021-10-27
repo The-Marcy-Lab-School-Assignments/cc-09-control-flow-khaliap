00de945e6a03bab9af77033d18f48e0f4075f59f
khalia <khaliaparris01@gmail.com>
2021-10-27 14:39:13 +0000

    finished code challenge

--- index.js
+++ index.js
@@ -1,30 +1,55 @@
 //1
-function betweenFiveAndTen(){
-  
+function betweenFiveAndTen(num){
+  if (num>=5 && num<=10){
+      return true
+  }else {
+      return false
+  }
 }
-
+// console.log(betweenFiveAndTen(20))
 //2
-function betweenTenAndThirty(){
-  
+function betweenTenAndThirty(num){
+   if (num>=10 && num<=30){
+      return true
+  }else {
+      return false
+  }
 }
-
+// console.log(betweenTenAndThirty(31))
 //3
-function betweenThirtyAndFifty(){
-  
+function betweenThirtyAndFifty(num){
+   if (num>=30 && num<=50){
+      return true
+  }else {
+      return false
+  }
 }
-
+// console.log(betweenThirtyAndFifty(55))
 
 //4
-function betweenThirtyFiftySeventy(){
-  
+function betweenThirtyFiftySeventy(num){
+   if (num>=0 && num<=30 ||num>=50 && num<=70 ){
+      return true
+  }else {
+      return false
+  }
 }
-
+// console.log(betweenThirtyFiftySeventy(60))
 //5
-function betweenTwentyFiftySixty(){
-  
+function betweenTwentyFiftySixty(num){
+   if (num>=0 && num<=20 ||num>=50 && num<=60 ){
+      return true
+  }else {
+      return false
+  }
 }
-
+// console.log(betweenTwentyFiftySixty(55))
 //6
-function betweenTenTwentyThirty(){
-  
+function betweenTenTwentyThirty(num){
+   if (num>=0 && num<=10 ||num>=20 && num<=30 ){
+      return true
+  }else {
+      return false
+  }
 }
+console.log(betweenTenTwentyThirty(21))
